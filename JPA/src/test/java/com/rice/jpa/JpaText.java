5a978640cc5d1200f7cb64381e24e500a732ad77
983378489 <983378489@qq.com>
2020-09-16 15:01:31 +0800

    JPA 改用Utils类生成EntityManager

--- JPA/src/test/java/com/rice/jpa/JpaText.java
+++ JPA/src/test/java/com/rice/jpa/JpaText.java
@@ -1,12 +1,11 @@
 package com.rice.jpa;
 
 import com.rice.jpa.entity.Customer;
+import com.rice.jpa.utils.JpaUtils;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 
 public class JpaText {
 
@@ -16,17 +15,26 @@ public class JpaText {
      *  Jpa的操作步骤
      *     1.加载配置文件创建工厂（实体管理器工厂）对象
      *     2.通过实体管理器工厂获取实体管理器
+     *          persist     保存
+     *          merge       更新
+     *          remove      删除
+     *          find/getRefrence     根据id查询
      *     3.获取事务对象，开启事务
+     *          begin       开启事务
+     * 			commit      提交事务
+     * 			rollback    回滚
      *     4.完成增删改查操作
      *     5.提交事务（回滚事务）
      *     6.释放资源
      */
     @Test
     public void testSave()  {
-        //1.加载配置文件创建工厂（实体管理器工厂）对象
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("myJpa");
-        //2.通过实体管理器工厂获取实体管理器
-        EntityManager em = factory.createEntityManager();
+//        //1.加载配置文件创建工厂（实体管理器工厂）对象
+//        EntityManagerFactory factory = Persistence.createEntityManagerFactory("myJpa");
+//        //2.通过实体管理器工厂获取实体管理器
+//        EntityManager em = factory.createEntityManager();
+        //从工具类里获取实体管理器
+        EntityManager em = JpaUtils.getEntityManager();
         //3.获取事务对象，开启事务
         //获取事务对象
         EntityTransaction tx = em.getTransaction();
@@ -34,13 +42,14 @@ public class JpaText {
         tx.begin();
         //4.完成增删改查操作：保存一个客户到数据库中
         Customer customer = new Customer();
-        customer.setCustName("马中原");
+        customer.setCustName("张铁");
         customer.setCustIndustry("IT");
-        //保存，
-        em.persist(customer); //保存操作
+        //保存操作
+        em.persist(customer);
         //5.提交事务
         tx.commit();
         //6.释放资源
         em.close();
+//        factory.close();
     }
 }
