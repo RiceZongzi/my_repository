86d9d1c9743468b87f065a0b84051f546a4d5cb7
983378489 <983378489@qq.com>
2020-09-17 15:53:05 +0800

    SpringDataJpa Jpa其他方法

--- SpringDataJpa/src/test/java/com/springdata/jpa/CustomerDaoTest.java
+++ SpringDataJpa/src/test/java/com/springdata/jpa/CustomerDaoTest.java
@@ -7,8 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext.xml")
@@ -60,5 +59,43 @@ public class CustomerDaoTest {
     @Test
     public void testFindAll(){
         customerDao.findAll().forEach(customer -> System.out.println(customer));
+    }
+
+    /**
+     * 统计数量
+     */
+    @Test
+    public void testCount(){
+        System.out.println(customerDao.count());
+    }
+
+    /**
+     * 测试：判断id为4的客户是否存在
+     *      1. 可以查询以下id为4的客户
+     *          如果值为空，代表不存在，如果不为空，代表存在
+     *      2. 判断数据库中id为4的客户的数量
+     *          如果数量为0，代表不存在，如果大于0，代表存在
+     */
+    @Test
+    public void testExists(){
+        // 这个采用的是上面的方案2
+        System.out.println(customerDao.exists(5L));
+    }
+
+    /**
+     * 根据id从数据库查询
+     *      @Transactional : 保证getOne正常运行
+     *
+     *  findOne：
+     *      em.find()           :立即加载
+     *  getOne：
+     *      em.getReference     :延迟加载
+     *      * 返回的是一个客户的动态代理对象
+     *      * 什么时候用，什么时候查询
+     */
+    @Test
+    @Transactional
+    public void testGetOne(){
+        System.out.println(customerDao.getOne(14L));
     }
 }
