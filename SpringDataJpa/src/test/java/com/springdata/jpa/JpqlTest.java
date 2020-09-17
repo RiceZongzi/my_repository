8bdbe9fa7c03e34cbc8e55dcf92c9f20d8746f99
983378489 <983378489@qq.com>
2020-09-17 18:11:13 +0800

    SpringDataJpa 查询方法

--- SpringDataJpa/src/test/java/com/springdata/jpa/JpqlTest.java
+++ SpringDataJpa/src/test/java/com/springdata/jpa/JpqlTest.java
@@ -1,6 +1,7 @@
 package com.springdata.jpa;
 
 import com.springdata.jpa.dao.CustomerDao;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,5 +14,13 @@ public class JpqlTest {
     @Autowired
     private CustomerDao customerDao;
 
+    @Test
+    public void testFindJPQL() {
+        System.out.println(customerDao.findJpql("马中原"));
+    }
 
+    @Test
+    public void testFindSQL() {
+        customerDao.findSql("马中原").forEach(objects -> System.out.println(objects));
+    }
 }
