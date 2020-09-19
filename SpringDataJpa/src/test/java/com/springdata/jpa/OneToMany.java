6fb1d42f4f3d40faccee81a7cf0ec267e37a08cd
983378489 <983378489@qq.com>
2020-09-19 16:15:35 +0800

    SpringDataJpa 一对多测试

--- SpringDataJpa/src/test/java/com/springdata/jpa/OneToMany.java
+++ SpringDataJpa/src/test/java/com/springdata/jpa/OneToMany.java
@@ -2,10 +2,15 @@ package com.springdata.jpa;
 
 import com.springdata.jpa.dao.CustomerDao;
 import com.springdata.jpa.dao.LinkManDao;
+import com.springdata.jpa.entity.Customer;
+import com.springdata.jpa.entity.LinkMan;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @Author ：by wgz
@@ -23,4 +28,20 @@ public class OneToMany {
 
     @Autowired
     private LinkManDao linkManDao;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void testAdd(){
+        Customer customer = new Customer();
+        LinkMan linkMan = new LinkMan();
+
+        customer.setCustName("Valve");
+        linkMan.setLkmName("Gabe");
+
+        customer.getLinkMans().add(linkMan);
+
+        customerDao.save(customer);
+        linkManDao.save(linkMan);
+    }
 }
