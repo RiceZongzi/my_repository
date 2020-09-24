cbf707d85d1967bcc789200a10c144919a0e6fe7
rice <983378489@qq.com>
2020-09-25 00:34:31 +0800

    Mybatis 由xml配置sql更改为注解配置sql

--- Mybatis/src/main/java/com/rice/dao/IUserDao.java
+++ Mybatis/src/main/java/com/rice/dao/IUserDao.java
@@ -1,10 +1,12 @@
 package com.rice.dao;
 
 import com.rice.entity.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface IUserDao {
 
+    @Select("select * from user ")
     List<User> findAll();
 }
