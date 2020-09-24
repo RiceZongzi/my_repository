16d4038cf646bbc1bfbf18a1fa3c3a06895daf96
rice <983378489@qq.com>
2020-09-25 02:13:50 +0800

    Mybatis CRUD part1

--- Mybatis/src/main/java/com/rice/dao/IUserDao.java
+++ Mybatis/src/main/java/com/rice/dao/IUserDao.java
@@ -6,5 +6,41 @@ import java.util.List;
 
 public interface IUserDao {
 
+    /**
+     * 查询所有用户
+     * @return
+     */
     List<User> findAll();
+
+    /**
+     * 保存用户
+     * @param user
+     */
+    void saveUser(User user);
+
+    /**
+     * 更新用户
+     * @param user
+     */
+    void updateUser(User user);
+
+    /**
+     * 根据Id删除用户
+     * @param userId
+     */
+    void deleteUser(Integer userId);
+
+    /**
+     * 根据id查询用户信息
+     * @param userId
+     * @return
+     */
+    User findById(Integer userId);
+
+    /**
+     * 根据名称模糊查询用户信息
+     * @param username
+     * @return
+     */
+    List<User> findByName(String username);
 }
--- Mybatis/src/test/java/com/rice/MybatisTest.java
+++ Mybatis/src/test/java/com/rice/MybatisTest.java
@@ -6,29 +6,95 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 
 public class MybatisTest {
 
-    public static void main(String[] args)throws Exception {
-        //1.读取配置文件
-        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
-        //2.创建SqlSessionFactory工厂
-        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory factory = builder.build(in);
-        //3.使用工厂生产SqlSession对象
-        SqlSession session = factory.openSession();
-        //4.使用SqlSession创建Dao接口的代理对象
-        IUserDao userDao = session.getMapper(IUserDao.class);
-        //5.使用代理对象执行方法
+    private InputStream in;
+    private SqlSession sqlSession;
+    private IUserDao userDao;
+
+    @Before
+    //用于在测试方法执行之前执行
+    public void init()throws Exception{
+        //1.读取配置文件，生成字节输入流
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        //2.获取SqlSessionFactory
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+        //3.获取SqlSession对象
+        sqlSession = factory.openSession();
+        //4.获取dao的代理对象
+        userDao = sqlSession.getMapper(IUserDao.class);
+    }
+
+    @After
+    //用于在测试方法执行之后执行
+    public void destroy()throws Exception{
+        //提交事务
+        sqlSession.commit();
+        //6.释放资源
+        sqlSession.close();
+        in.close();
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
         List<User> users = userDao.findAll();
         for(User user : users){
             System.out.println(user);
         }
-        //6.释放资源
-        session.close();
-        in.close();
+    }
+
+    @Test
+    public void testSaveUser() throws Exception {
+        User user = new User();
+        user.setUsername("玛吉纳");
+        user.setAddress("曙光酒馆");
+        user.setSex("男");
+        user.setBirthday(new Date());
+
+        //5.使用代理对象执行方法
+        userDao.saveUser(user);
+    }
+
+    @Test
+    public void testUpdate(){
+        User user = new User();
+        user.setId(49);
+        user.setUsername("玛吉纳");
+        user.setAddress("曙光酒馆");
+        user.setSex("男");
+        user.setBirthday(new Date());
+
+        //5.执行保存方法
+        userDao.updateUser(user);
+    }
+
+    @Test
+    public void testDelete(){
+        //5.执行删除方法
+        userDao.deleteUser(49);
+    }
+
+    @Test
+    public void testFindOne(){
+        //5.执行查询一个方法
+        User user = userDao.findById(41);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testFindByName(){
+        //5.执行查询一个方法
+        List<User> users = userDao.findByName("%王%");
+        for(User user : users){
+            System.out.println(user);
+        }
     }
 }
