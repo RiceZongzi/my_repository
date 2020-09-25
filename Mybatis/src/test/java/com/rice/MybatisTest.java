2f106aee365db14e4d9a0d641d71d3f66119de16
983378489 <983378489@qq.com>
2020-09-25 10:56:16 +0800

    MyBatis 抽取测试类的公共部分

--- Mybatis/src/test/java/com/rice/MybatisTest.java
+++ Mybatis/src/test/java/com/rice/MybatisTest.java
@@ -1,34 +1,47 @@
 package com.rice;
 
 import com.rice.dao.IUserDao;
-import com.rice.entity.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.InputStream;
-import java.util.List;
 
 public class MybatisTest {
 
-    public static void main(String[] args)throws Exception {
-        //1.读取配置文件
-        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
-        //2.创建SqlSessionFactory工厂
-        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory factory = builder.build(in);
-        //3.使用工厂生产SqlSession对象
-        SqlSession session = factory.openSession();
-        //4.使用SqlSession创建Dao接口的代理对象
-        IUserDao userDao = session.getMapper(IUserDao.class);
-        //5.使用代理对象执行方法
-        List<User> users = userDao.findAll();
-        for(User user : users){
-            System.out.println(user);
-        }
+    private InputStream in;
+    private SqlSession sqlSession;
+    private IUserDao userDao;
+
+    @Before
+    //用于在测试方法执行之前执行
+    public void init()throws Exception{
+        //1.读取配置文件，生成字节输入流
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        //2.获取SqlSessionFactory
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+        //3.获取SqlSession对象
+        sqlSession = factory.openSession();
+        //4.获取dao的代理对象
+        userDao = sqlSession.getMapper(IUserDao.class);
+    }
+
+    @After
+    //用于在测试方法执行之后执行
+    public void destroy()throws Exception{
+        //提交事务
+        sqlSession.commit();
         //6.释放资源
-        session.close();
+        sqlSession.close();
         in.close();
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+        userDao.findAll().forEach(user -> System.out.println(user));
     }
 }
