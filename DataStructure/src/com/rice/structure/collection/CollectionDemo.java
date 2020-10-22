fb90b0f2182a5acab2e509b79a69c2c637c2f775
rice <983378489@qq.com>
2020-10-22 20:11:54 +0800

    Collection

--- DataStructure/src/com/rice/structure/collection/CollectionDemo.java
+++ DataStructure/src/com/rice/structure/collection/CollectionDemo.java
@@ -19,7 +19,7 @@ public class CollectionDemo {
      *
      *
      *     共性的方法:
-     *       public boolean add(E e)：  把给定的对象添加到当前集合中 。
+     *       public boolean add(E e)：  把给定的对象添加到当前集合中。
      *       public void clear() :清空集合中所有的元素。
      *       public boolean remove(E e): 把给定的对象在当前集合中删除。
      *       public boolean contains(E e): 判断当前集合中是否包含给定的对象。
