9c38f8f247e957b941efcc12207bbb39275a226f
983378489 <983378489@qq.com>
2020-10-26 17:20:14 +0800

    Thread 匿名内部类改成lambda表达式

--- Thread/src/com/rice/demo/ThreadDemo.java
+++ Thread/src/com/rice/demo/ThreadDemo.java
@@ -42,76 +42,67 @@ public class ThreadDemo {
         // 创建锁对象,保证唯一
         Object obj = new Object();
         // 创建一个顾客线程(消费者)
-        new Thread(){
-            @Override
-            public void run() {
-                // 一直等着买包子
-                while(true){
-                    // 保证等待和唤醒的线程只能有一个执行,需要使用同步技术
-                    synchronized (obj){
-                        System.out.println("顾客1告知老板要的包子的种类和数量");
-                        // 调用wait方法,放弃cpu的执行,进入到WAITING状态(无限等待)
-                        try {
-                            obj.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        // 唤醒之后执行的代码
-                        System.out.println("包子已经做好了,顾客1开吃!");
-                        System.out.println("---------------------------------------");
-                    }
-                }
-            }
-        }.start();
-
-        // 创建一个顾客线程(消费者)
-        new Thread(){
-            @Override
-            public void run() {
-                // 一直等着买包子
-                while(true){
-                    // 保证等待和唤醒的线程只能有一个执行,需要使用同步技术
-                    synchronized (obj){
-                        System.out.println("顾客2告知老板要的包子的种类和数量");
-                        // 调用wait方法,放弃cpu的执行,进入到WAITING状态(无限等待)
-                        try {
-                            obj.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        // 唤醒之后执行的代码
-                        System.out.println("包子已经做好了,顾客2开吃!");
-                        System.out.println("---------------------------------------");
-                    }
-                }
-            }
-        }.start();
-
-        // 创建一个老板线程(生产者)
-        new Thread(){
-            @Override
-            public void run() {
-                // 一直做包子
-                while (true){
+        new Thread(() -> {
+            // 一直等着买包子
+            while(true){
+                // 保证等待和唤醒的线程只能有一个执行,需要使用同步技术
+                synchronized (obj){
+                    System.out.println("顾客1告知老板要的包子的种类和数量");
+                    // 调用wait方法,放弃cpu的执行,进入到WAITING状态(无限等待)
                     try {
-                        // 花了5秒做包子
-                        Thread.sleep(5000);
+                        obj.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-
-                    // 保证等待和唤醒的线程只能有一个执行,需要使用同步技术
-                    synchronized (obj){
-                        System.out.println("老板5秒钟之后做好包子,告知顾客,可以吃包子了");
-                        // 做好包子之后,调用notify方法,唤醒顾客吃包子
-                        // 如果有多个等待线程,随机唤醒一个
-                        // obj.notify();
-                        // 唤醒所有等待的线程
-                        obj.notifyAll();
-                    }
+                    // 唤醒之后执行的代码
+                    System.out.println("包子已经做好了,顾客1开吃!");
+                    System.out.println("---------------------------------------");
                 }
             }
-        }.start();
+        }).start();
+
+        // 创建一个顾客线程(消费者)
+        new Thread(() -> {
+            // 一直等着买包子
+            while(true){
+                // 保证等待和唤醒的线程只能有一个执行,需要使用同步技术
+                synchronized (obj){
+                    System.out.println("顾客2告知老板要的包子的种类和数量");
+                    // 调用wait方法,放弃cpu的执行,进入到WAITING状态(无限等待)
+                    try {
+                        obj.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    // 唤醒之后执行的代码
+                    System.out.println("包子已经做好了,顾客2开吃!");
+                    System.out.println("---------------------------------------");
+                }
+            }
+        }).start();
+
+        // 创建一个老板线程(生产者)
+        new Thread(() -> {
+            // 一直做包子
+            while (true){
+                try {
+                    // 花了5秒做包子
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                // 保证等待和唤醒的线程只能有一个执行,需要使用同步技术
+                synchronized (obj){
+                    System.out.println("老板5秒钟之后做好包子,告知顾客,可以吃包子了");
+                    // 做好包子之后,调用notify方法,唤醒顾客吃包子
+                    // 如果有多个等待线程,随机唤醒一个
+                    // obj.notify();
+                    // 唤醒所有等待的线程
+                    obj.notifyAll();
+                }
+            }
+        }).start();
     }
 
     /**
@@ -135,50 +126,44 @@ public class ThreadDemo {
         // 创建锁对象,保证唯一
         Object obj = new Object();
         // 创建一个顾客线程(消费者)
-        new Thread(){
-            @Override
-            public void run() {
-                // 一直等着买包子
-                while(true){
-                    // 保证等待和唤醒的线程只能有一个执行,需要使用同步技术
-                    synchronized (obj){
-                        System.out.println("告知老板要的包子的种类和数量");
-                        // 调用wait方法,放弃cpu的执行,进入到WAITING状态(无限等待)
-                        try {
-                            obj.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        // 唤醒之后执行的代码
-                        System.out.println("包子已经做好了,开吃!");
-                        System.out.println("---------------------------------------");
-                    }
-                }
-            }
-        }.start();
-
-        // 创建一个老板线程(生产者)
-        new Thread(){
-            @Override
-            public void run() {
-                // 一直做包子
-                while (true){
+        new Thread(() -> {
+            // 一直等着买包子
+            while(true){
+                // 保证等待和唤醒的线程只能有一个执行,需要使用同步技术
+                synchronized (obj){
+                    System.out.println("告知老板要的包子的种类和数量");
+                    // 调用wait方法,放弃cpu的执行,进入到WAITING状态(无限等待)
                     try {
-                        // 花了5秒做包子
-                        Thread.sleep(5000);
+                        obj.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-
-                    // 保证等待和唤醒的线程只能有一个执行,需要使用同步技术
-                    synchronized (obj){
-                        System.out.println("老板5秒钟之后做好包子,告知顾客,可以吃包子了");
-                        // 做好包子之后,调用notify方法,唤醒顾客吃包子
-                        obj.notify();
-                    }
+                    // 唤醒之后执行的代码
+                    System.out.println("包子已经做好了,开吃!");
+                    System.out.println("---------------------------------------");
                 }
             }
-        }.start();
+        }).start();
+
+        // 创建一个老板线程(生产者)
+        new Thread(() -> {
+            // 一直做包子
+            while (true){
+                try {
+                    // 花了5秒做包子
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                // 保证等待和唤醒的线程只能有一个执行,需要使用同步技术
+                synchronized (obj){
+                    System.out.println("老板5秒钟之后做好包子,告知顾客,可以吃包子了");
+                    // 做好包子之后,调用notify方法,唤醒顾客吃包子
+                    obj.notify();
+                }
+            }
+        }).start();
     }
 
     private static void method7() {
@@ -280,14 +265,10 @@ public class ThreadDemo {
      */
     private static void method3() {
 
-        Runnable r = new Runnable(){
-
-            @Override
-            public void run() {
-                System.out.println("This is NoNameInnerClass");
-                for (int i = 0; i < 50; i++) {
-                    System.out.println("NoNameInnerClass" + i);
-                }
+        Runnable r = () -> {
+            System.out.println("This is NoNameInnerClass");
+            for (int i = 0; i < 50; i++) {
+                System.out.println("NoNameInnerClass" + i);
             }
         };
 
