940fe37eddf1cde222b77e7f374b685dd570ae6a
983378489 <983378489@qq.com>
2020-10-27 17:22:55 +0800

    JDK8 Comparator的compare方法

--- JDK8/src/com/rice/lambda/LambdaDemo.java
+++ JDK8/src/com/rice/lambda/LambdaDemo.java
@@ -1,5 +1,8 @@
 package com.rice.lambda;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 /**
  * @author wgz
  * @description
@@ -7,8 +10,43 @@ package com.rice.lambda;
  */
 public class LambdaDemo {
 
+    /**
+    Lambda表达式的标准格式:
+        由三部分组成:
+            a.一些参数
+            b.一个箭头
+            c.一段代码
+        格式:
+            (参数列表) -> {一些重写方法的代码};
+        解释说明格式:
+            ():接口中抽象方法的参数列表,没有参数,就空着;有参数就写出参数,多个参数使用逗号分隔
+            ->:传递的意思,把参数传递给方法体{}
+            {}:重写接口的抽象方法的方法体
+    */
     public static void main(String[] args) {
-        showDiffByWaysToCreateThread();
+//        showDiffByWaysToCreateThread();
+        sortObject();
+    }
+
+    private static void sortObject() {
+        ArrayList<Person> arrayList = new ArrayList<>();
+        arrayList.add(new Person("AntiMage", 26));
+        arrayList.add(new Person("Viper", 46));
+        arrayList.add(new Person("Invoker", 22));
+        arrayList.add(new Person("Axe", 30));
+        arrayList.add(new Person("Luna", 21));
+        // 匿名内部类
+        arrayList.sort(new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getAge() - o2.getAge();
+            }
+        });
+        // Lambda @Since JDK 8
+        arrayList.sort((o1, o2) -> o1.getAge() - o2.getAge());
+        // Functional @Since JDK 8
+        arrayList.sort(Comparator.comparingInt(Person::getAge));
+        System.out.println(arrayList);
     }
 
     private static void showDiffByWaysToCreateThread() {
@@ -38,5 +76,43 @@ public class LambdaDemo {
 
 //        By lambda expression
         new Thread(() -> System.out.println(Thread.currentThread().getName() + " Lambda expression 线程创建了")).start();
+    }
+}
+
+class Person {
+    private String name;
+
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
