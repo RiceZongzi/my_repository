df47d4590e15eb6501119b8b44979b498a81589e
983378489 <983378489@qq.com>
2020-10-27 10:58:22 +0800

    JDK8 First

---
+++ JDK8/src/com/rice/lambda/LambdaDemo.java
@@ -0,0 +1,9 @@
+package com.rice.lambda;
+
+/**
+ * @author wgz
+ * @description
+ * @date 2020/10/27 10:56
+ */
+public class LambdaDemo {
+}
---
+++ JDK8/src/com/rice/stream/StreamDemo.java
@@ -0,0 +1,9 @@
+package com.rice.stream;
+
+/**
+ * @author wgz
+ * @description
+ * @date 2020/10/27 10:57
+ */
+public class StreamDemo {
+}
