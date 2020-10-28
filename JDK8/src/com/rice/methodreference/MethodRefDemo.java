93c3fde8c29de25bf7db5b9afdf2f2e0bfbbaf2e
rice <983378489@qq.com>
2020-10-28 23:34:12 +0800

    JDK8 ThisMethodReference

--- JDK8/src/com/rice/methodreference/MethodRefDemo.java
+++ JDK8/src/com/rice/methodreference/MethodRefDemo.java
@@ -225,3 +225,50 @@ interface Greetable {
      */
     void greet();
 }
+
+/**
+ * 定义一个富有的函数式接口
+ */
+@FunctionalInterface
+interface Richable {
+    /**
+     * 定义一个想买什么就买什么的方法
+     * @author wgz
+     * @date 2020/10/28
+     */
+    void buy();
+}
+
+class Husband {
+    public void buyHouse(){
+        // 定义一个买房子的方法
+        System.out.println("北京二环内买一套四合院!");
+    }
+
+    public void marry(Richable r){
+        // 定义一个结婚的方法,参数传递Richable接口
+        r.buy();
+    }
+
+    public void soHappy(){
+        // 调用结婚的方法
+        // 方法的参数Richable是一个函数式接口
+        // 传递Lambda表达式
+        marry(()->{
+            // 使用this.成员方法，调用本类买房子的方法
+            this.buyHouse();
+        });
+
+        /*
+            使用方法引用优化Lambda表达式
+            this是已经存在的
+            本类的成员方法buyHouse也是已经存在的
+            所以我们可以直接使用this引用本类的成员方法buyHouse
+         */
+        marry(this::buyHouse);
+    }
+
+    public static void main(String[] args) {
+        new Husband().soHappy();
+    }
+}
