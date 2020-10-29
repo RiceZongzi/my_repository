9d63c2690ae99ab4741348872d4416d9fc7875eb
rice <983378489@qq.com>
2020-10-30 01:35:38 +0800

    Optional part04

--- JDK8/src/com/rice/optional/OptionalDemo.java
+++ JDK8/src/com/rice/optional/OptionalDemo.java
@@ -18,9 +18,11 @@ public class OptionalDemo {
         isPresent() : 判断是否包含值
         ifPresent(Consumer<? super T> consumer) : 如果值存在则使用该值调用 consumer，否则不做任何事情。
         orElse(T t) : 如果调用对象包含值，返回该值，否则返回t
-        orElseGet(Supplier s) : 如果调用对象包含值，返回该值，否则返回s获取的值
-        map(Function f) : 如果有值对其处理，并返回处理后的Optional，否则返回Optional.empty()
-        flatMap(Function mapper) : 与 map类似，要求返回值必须是Optional
+        orElseGet(Supplier<? extends T> other) : 如果存在该值，返回值， 否则触发 other，并返回 other 调用的结果。
+        orElseThrow(Supplier<? extends X> exceptionSupplier) ： 如果存在该值，返回包含的值，否则抛出由 Supplier 继承的异常
+        map(Function<? super T,? extends U> mapper) : 如果有值，则对其执行调用映射函数得到返回值。如果返回值不为 null，则创建包含映射返回值的Optional作为map方法返回值，否则返回空Optional。
+        flatMap(Function<? super T,Optional<U>> mapper) : 如果值存在，返回基于Optional包含的映射方法的值，否则返回一个空的Optional
+        filter(Predicate<? super <T> predicate) : 如果值存在，并且这个值匹配给定的 predicate，返回一个Optional用以描述这个值，否则返回一个空的Optional。
     */
     public static void main(String[] args) {
 //        construction();
@@ -28,8 +30,42 @@ public class OptionalDemo {
 //        test01(new Student("Axe", "Dire", 40));
 //        test02(null);
 //        test02(new Student("Axe", "Dire", 40));
-        test03(null);
-        test03(new Student("Axe", "Dire", 40));
+//        test03(null);
+//        test03(new Student("Axe", "Dire", 40));
+        test04(null);
+        test04(new Man());
+        test04(new Man(new Goddess("Athena")));
+        // Hera
+        test05(Optional.ofNullable(null));
+        // Hera
+        test05(Optional.ofNullable(new OptionalMan()));
+        // Athena
+        test05(Optional.ofNullable(new OptionalMan(Optional.ofNullable((new Goddess("Athena"))))));
+    }
+
+    private static void test05(Optional<OptionalMan> man) {
+        System.out.println(getOptionalGoddessName(man));
+    }
+
+    private static void test04(Man man) {
+        System.out.println(getGoddessName(man));
+    }
+
+    private static String getGoddessName(Man man) {
+        if (man != null) {
+            Goddess goddess = man.getGoddess();
+            if (goddess != null) {
+                return goddess.getName();
+            }
+        }
+        return "Hera";
+    }
+
+    private static String getOptionalGoddessName(Optional<OptionalMan> man) {
+        return man.orElse(new OptionalMan())
+                .getGoddess()
+                .orElse(new Goddess("Hera"))
+                .getName();
     }
 
     private static void test03(Student student) {
@@ -136,6 +172,84 @@ class Student {
                 "name='" + name + '\'' +
                 ", address='" + address + '\'' +
                 ", age=" + age +
+                '}';
+    }
+}
+
+class Man {
+    private Goddess goddess;
+
+    public Goddess getGoddess() {
+        return goddess;
+    }
+
+    public void setGoddess(Goddess goddess) {
+        this.goddess = goddess;
+    }
+
+    public Man(Goddess goddess) {
+        this.goddess = goddess;
+    }
+
+    public Man() {
+    }
+
+    @Override
+    public String toString() {
+        return "Man{" +
+                "goddess=" + goddess +
+                '}';
+    }
+}
+
+class OptionalMan {
+    private Optional<Goddess> goddess = Optional.empty();
+
+    public Optional<Goddess> getGoddess() {
+        return goddess;
+    }
+
+    public void setGoddess(Optional<Goddess> goddess) {
+        this.goddess = goddess;
+    }
+
+    public OptionalMan(Optional<Goddess> goddess) {
+        this.goddess = goddess;
+    }
+
+    public OptionalMan() {
+    }
+
+    @Override
+    public String toString() {
+        return "OptionalMan{" +
+                "goddess=" + goddess +
+                '}';
+    }
+}
+
+class Goddess {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Goddess(String name) {
+        this.name = name;
+    }
+
+    public Goddess() {
+    }
+
+    @Override
+    public String toString() {
+        return "Goddess{" +
+                "name='" + name + '\'' +
                 '}';
     }
 }
