0b896845bd401fe4034b6a6e9ecbed17749e47eb
983378489 <983378489@qq.com>
2020-10-29 10:46:45 +0800

    JDK8 NewTimeAPI LocalDate

--- JDK8/src/com/rice/time/NewTimeAPIDemo.java
+++ JDK8/src/com/rice/time/NewTimeAPIDemo.java
@@ -1,9 +1,46 @@
 package com.rice.time;
 
+import java.time.LocalDate;
+
 /**
  * @author wgz
  * @description
  * @date 2020/10/29 10:03
  */
 public class NewTimeAPIDemo {
+
+    public static void main(String[] args) {
+        localDate();
+    }
+
+    /**
+     * 类获取日期信息，格式为 yyyy-MM-dd
+     * @author wgz
+     * @date 2020/10/29
+     */
+    private static void localDate() {
+        // yyyy-MM-dd 格式的日期
+        LocalDate today = LocalDate.now();
+        System.out.println("今天的日期是：" + today);
+        // 获取年的常用方法
+        int year = today.getYear();
+        // 获取月的常用方法，
+        int monthValue = today.getMonthValue();
+        // 获取日的常用方法，返回的是当月的第几天
+        int day = today.getDayOfMonth();
+        System.out.println("今天的日期是："
+                + year + "年"
+                + monthValue + "月"
+                + day + "日");
+
+        // 当年的第几天
+        int dayOfYear = today.getDayOfYear();
+        System.out.println("是今年的第" + dayOfYear + "天");
+        // 星期
+        System.out.println(today.getDayOfWeek());
+        System.out.println(today.getDayOfWeek().getValue());
+        // 月份，today.getMonth() 方法返回的是一个枚举类的值
+        System.out.println(today.getMonth());
+        System.out.println(today.getMonth().getValue());
+    }
 }
