f9ded2ec8d81e9447a3359a253240c1ad652a820
983378489 <983378489@qq.com>
2020-10-30 11:09:06 +0800

    JDK8 Nashron First

---
+++ JDK8/src/com/rice/nashron/NashronDemo.java
@@ -0,0 +1,14 @@
+package com.rice.nashron;
+
+/**
+ * @author wgz
+ * @description
+ * @date 2020/10/30 11:07
+ */
+public class NashronDemo {
+
+    public static void main(String[] args) {
+
+
+    }
+}
