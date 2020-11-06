93ec01623b89e863e7c97db301e8a5b942dc99d6
983378489 <983378489@qq.com>
2020-11-06 16:00:08 +0800

    Lucene First

---
+++ Lucene/src/com/rice/lucene/LuceneDemo.java
@@ -0,0 +1,23 @@
+package com.rice.lucene;
+
+import org.junit.Test;
+
+/**
+ * @author wgz
+ * @description
+ * @date 2020/11/6 14:17
+ */
+public class LuceneDemo {
+
+    @Test
+    public void createIndex() {
+        //1 创建文档对象
+        //2 创建存储目录
+        //3 创建分词器
+        //4 创建索引写入器的配置对象
+        //5 创建索引写入器对象
+        //6 将文档交给索引写入器
+        //7 提交
+        //8 关闭
+    }
+}
